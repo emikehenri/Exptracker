ae522823000d097b5ec64ce27b23ef6df2fc0ef1
emikehenri <101247994+emikehenri@users.noreply.github.com>
2025-01-05 19:01:20 +0800

    updating design of dashboard

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-let exp = document.querySelector('#exp');
+document.addEventListener('DOMContentLoaded', () => {
+const active_hover = () =>{
+    let hover = document.querySelectorAll('#activepage');
+
+     console.log(hover);
+    hover.forEach(link => {
+        link.addEventListener('click', () => {
+            let actives = window.location.pathname;
+            let links = link.getAttribute('href');
+
+            if(actives == links){  
+                link.classList.add('active', 'text-primary') 
+            } 
+            else 
+            {
+                link.classList.remove('active')
+            }
+            
+         });
+    })
+}
+active_hover();
+})
